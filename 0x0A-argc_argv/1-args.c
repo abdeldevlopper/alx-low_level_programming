a0867a6d6d623f229f71a5a93adf7701edae789b
Abdelhafid <Abdeldevlopper@gmail.com>
2023-04-10 08:59:04 -0700

    Adding a new 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints the number of arguments.
- * @argc: the number of arguments.
- * @argv: the array of arguments.
- * Return: always 0.
- */
-
-int main(int argc, char const *argv[])
+ * main - This program prints the number of arguments passed into it
+ *
+ * @argc: number of arguments
+ * @argv: arguments vector
+ *
+ * Return: 0 if success
+*/
+int main(int argc, __attribute__((unused)) char **argv)
 {
-        printf("%d\n", argc - 1);
-        return (0);
+	printf("%d\n", argc - 1);
+
+	return  (0);
 }
