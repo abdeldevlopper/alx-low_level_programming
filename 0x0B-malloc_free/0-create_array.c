19270aea354ea4e05916ea5f6af3fb3131d11bf4
Abdelhafid <Abdeldevlopper@gmail.com>
2023-04-11 15:02:32 -0700

    a new fille called 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars, and initializes it with char c
  *
  * @size: the size of the array to create
  * @c: the char to initialize the array with
  *
  * Return: On success, a pointer to the newly allocated array
- * On failure, NULL
+ *         On failure, NULL
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr = NULL;
-    unsigned int i;
+char *arr = NULL;
+unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
-        return (NULL);
+arr = malloc(size * sizeof(char));
+if (arr == NULL)
+return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
+for (i = 0; i < size; i++)
+arr[i] = c;
 
-    return (arr);
+return (arr);
 }
