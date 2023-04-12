a210c1ffb67868c1dcb53a5e38335aece6fc3ec3
Abdelhafid <Abdeldevlopper@gmail.com>
2023-04-11 17:40:01 -0700

    New fille called 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,9 +6,11 @@
  * @grid: the 2D array freed
  * @height: the height of the array
  */
+
 void free_grid(int **grid, int height)
 {
     int i;
+
     for (i = 0; i < height; i++)
     {
         free(grid[i]);
