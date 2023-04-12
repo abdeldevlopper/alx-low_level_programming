e5cf8902c89364ca54eb21ea376892a76ff59566
Abdelhafid <Abdeldevlopper@gmail.com>
2023-04-11 17:27:14 -0700

    New fille called 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,37 +7,36 @@
  * @height: the height of the array.
  * Return: the 2D array created, or NULL on failure.
  */
+
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int row, col;
+int **grid;
+int row, col;
 
-    /* Check for invalid input */
-    if (width <= 0 || height <= 0)
-        return (NULL);
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
-    /* Allocate memory for the rows */
-    grid = malloc(sizeof(int *) * height);
-    if (grid == NULL)
-        return (NULL);
+grid = malloc(sizeof(int *) * height);
+   
+if (grid == NULL)
+{
+return (NULL);
+}
 
-    /* Allocate memory for the columns of each row */
-    for (row = 0; row < height; row++)
-    {
-        grid[row] = malloc(sizeof(int) * width);
-        if (grid[row] == NULL)
-        {
-            /* Free all previously allocated memory on failure */
-            for (col = 0; col < row; col++)
-                free(grid[col]);
-            free(grid);
-            return (NULL);
-        }
-
-        /f Initialize all elements to 0 */
-        for (col = 0; col < width; col++)
-            grid[row][col] = 0;
-    }
-
-    return (grid);
+for (row = 0; row < height; row++)
+{
+grid[row] = malloc(sizeof(int) * width);
+if (grid[row] == NULL)
+{
+for (col = 0; col < row; col++)
+free(grid[col]);
+free(grid);
+return (NULL);
+}
+}
+for (col = 0; col < width; col++)
+grid[row][col] = 0;
+return (grid);
 }
