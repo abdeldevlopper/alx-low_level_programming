5b72def763b4f7a0b63cb23c7d441be141c12157
Abdelhafid <Abdeldevlopper@gmail.com>
2023-04-21 09:50:52 -0700

    Adding a new fille called 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,59 +1,49 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * print_all - prints a variable number of arguments based on a given format
- * @format: string containing the format of the arguments
- * @...: variable number of arguments
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-        const char *ptr = format;
-        va_list args;
-        int i = 0;
-        char c;
-        char *s;
-        float f;
+    int i = 0;
+    char *str, *sep = "";
+    va_list list;
 
-        va_start(args, format);
+    va_start(list, format);
 
-        while (ptr && ptr[i])
+    if (format)
+    {
+        while (format[i])
         {
-                switch (ptr[i])
-                {
-                        case 'c':
-                                c = va_arg(args, int);
-                                printf("%c", c);
-                                break;
-
-                        case 'i':
-                                i = va_arg(args, int);
-                                printf("%d", i);
-                                break;
-
-                        case 'f':
-                                f = va_arg(args, double);
-                                printf("%f", f);
-                                break;
-
-                        case 's':
-                                s = va_arg(args, char *);
-                                if (s == NULL)
-                                {
-                                        printf("(nil)");
-                                        break;
-                                }
-                                printf("%s", s);
-                                break;
-
-                        default:
-                                break;
-                }
-
-                if (ptr[i + 1])
-                        printf(", ");
-                i++;
+            switch (format[i])
+            {
+                case 'c':
+                    printf("%s%c", sep, va_arg(list, int));
+                    break;
+                case 'i':
+                    printf("%s%d", sep, va_arg(list, int));
+                    break;
+                case 'f':
+                    printf("%s%f", sep, va_arg(list, double));
+                    break;
+                case 's':
+                    str = va_arg(list, char *);
+                    if (!str)
+                        str = "(nil)";
+                    printf("%s%s", sep, str);
+                    break;
+                default:
+                    i++;
+                    continue;
+            }
+            sep = ", ";
+            i++;
         }
-        va_end(args);
-        printf("\n");
+    }
+
+    printf("\n");
+    va_end(list);
 }
