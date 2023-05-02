509aab3f4f3594fb913b6cb17d00b6a326ef7483
abdeldevlopper <abdel.bouy03@gmail.com>
2023-05-02 15:49:47 -0700

    Adding a new fille called 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-    int n;
-    listint_t *temp;
+	int n;
+	listint_t *temp;
 
-    if (*head == NULL)
-        return (0);
+	if (*head == NULL)
+		return (0);
 
-    n = (*head)->n;
-    temp = *head;
-    *head = (*head)->next;
-    free(temp);
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
 
-    return (n);
+	return (n);
 }
