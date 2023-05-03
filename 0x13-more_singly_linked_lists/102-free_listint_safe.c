5fb61956f2114fb05520e154a73ff7c2cf74d2ab
abdeldevlopper <abdel.bouy03@gmail.com>
2023-05-02 18:50:32 -0700

    Adding a new fille called 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,41 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_listint_safe - frees a linked list
- * @head: pointer to the first node in the linked list
+ * @h: pointer to the first node in the linked list
  *
  * Return: number of elements in the freed list
  */
-size_t free_listint_safe(listint_t **head)
+size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	int diff;
-	listint_t *temp;
+        size_t len = 0;
+        int diff;
+        listint_t *temp;
 
-	if (!head || !*head)
-		return (0);
+        if (h == NULL || *h == NULL)
+                return (0);
 
-	while (*head)
-	{
-		diff = *head - (*head)->next;
-		if (diff > 0)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-			count++;
-		}
-		else
-		{
-			free(*head);
-			*head = NULL;
-			count++;
-			break;
-		}
-	}
+        while (*h != NULL)
+        {
+                diff = *h - (*h)->next;
+                if (diff > 0)
+                {
+                        temp = (*h)->next;
+                        free(*h);
+                        *h = temp;
+                        len++;
+                }
+                else
+                {
+                        free(*h);
+                        *h = NULL;
+                        len++;
+                        break;
+                }
+        }
 
-	*head = NULL;
+        *h = NULL;
 
-	return (count);
+        return (len);
 }
